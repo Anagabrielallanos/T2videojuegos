1f40cdf9edbf609b73aba8e85eba72f9930dc0b1
Anagabrielallanos <vaale.gby.28@gmail.com>
2020-10-16 13:25:22 -0500

    first commit

---
+++ Assets/Scripts/Zombie2Controller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie2Controller : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator animator;
+    public float velocidad = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(velocidad, rb.velocity.y);
+        sr.flipX = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            animator.SetBool("EstaAtacando", true);
+        }
+        else
+        {
+            animator.SetBool("EstaAtacando", false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    public GameObject enemigo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("CrearEnemigo", (Random.Range(3, 15)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void CrearEnemigo()
+    {
+        Instantiate(enemigo, transform.position, Quaternion.identity);
+        float delay = Random.Range(5, 20);
+        Invoke("CrearEnemigo", delay);
+    }
+}
